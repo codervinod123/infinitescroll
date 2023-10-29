d9bf2ec55854f84b85954d5a07d4312e00ab557f
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2023-10-29 17:23:58 +0530

    implemented infinite scroll

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,16 +7,31 @@ const Home = () => {
     const [data,setData]=useState([]);
     const [pageNo,setPageNo]=useState(1);
     const getData=async()=>{
-       const data=await fetch("https://jsonplaceholder.typicode.com/posts?_limit=9&_page="+pageNo);
+       const data=await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=9&_page=${pageNo}`);
        const json=await data.json();
-       setData(json);
+       setData((prev)=>[...prev,...json]);
     }
     
     useEffect(()=>{
         getData();
-    },[])
-    
+    },[pageNo])
 
+    const handleScroll=async()=>{
+       const total=document.documentElement.scrollHeight;
+       const actualHeight=window.innerHeight;
+       const scrolled=document.documentElement.scrollTop;
+       try {
+           if(actualHeight+scrolled+1>total){
+              setPageNo((prev)=>prev+1);
+           }
+       } catch (error) {
+          console.log(error);
+       }
+    }
+    
+    useEffect(()=>{
+       window.addEventListener('scroll',handleScroll);
+    },[])
     
 
   return (
